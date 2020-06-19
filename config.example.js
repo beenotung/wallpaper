bd1ac1e2edefd453d70ad3f0c49261f0a69b2faf
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-06-19 10:11:18 +0800

    resolve boundary bug in the sample config

--- config.example.js
+++ config.example.js
@@ -14,10 +14,10 @@ let hackathon = img('hackathon.jpg')
 module.exports = {
   default: other,
   slots: [
-    { hour: '5-10', file: morning },
-    { hour: '10-18', file: focus_work },
+    { hour: '5-9', file: morning },
+    { hour: '10-17', file: focus_work },
     { hour: '18-19', file: care_home },
-    { hour: '19-22', file: busy_city },
+    { hour: '20-22', file: busy_city },
     { day_of_week: '0,6', file: hackathon },
   ],
 }
