d7cf6bf25b41c84f055133d61c8263380aa9cfa7
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-06-27 11:59:49 +0800

    resolve conflict on weekday and weekend setting

--- config.example.js
+++ config.example.js
@@ -8,16 +8,16 @@ let other = img('relax.jpg')
 let focus_work = img('work.jpg')
 let care_home = img('wife.jpg')
 let busy_city = img('highway.jpg')
-let morning = img('dawn.jpg')
+let dawn_city = img('dawn.jpg')
 let hackathon = img('hackathon.jpg')
 
 module.exports = {
   default: other,
   slots: [
-    { hour: '5-9', file: morning },
-    { hour: '10-17', file: focus_work },
-    { hour: '18-19', file: care_home },
-    { hour: '20-22', file: busy_city },
+    { day_of_week: '1-5', hour: '5-9', file: dawn_city },
+    { day_of_week: '1-5', hour: '10-17', file: focus_work },
+    { day_of_week: '1-5', hour: '18-19', file: care_home },
+    { day_of_week: '1-5', hour: '20-22', file: busy_city },
     { day_of_week: '0,6', file: hackathon },
   ],
 }
